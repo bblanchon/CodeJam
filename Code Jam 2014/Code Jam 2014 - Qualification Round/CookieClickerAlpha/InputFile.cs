27fc16039d36107b1771b38e903041f841f7a4ea
Benoit Blanchon <benoit.blanchon@gmail.com>
2014-04-12 15:00:12 +0200

    Added qualification 2014, problem B

---
+++ Code Jam 2014/Code Jam 2014 - Qualification Round/CookieClickerAlpha/InputFile.cs
@@ -0,0 +1,52 @@
+﻿// Copyright © Benoit Blanchon 2014
+// All Rights Reserved
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace CookieClickerAlpha
+{
+    class InputFile
+    {
+        public InputFile(string filePath)
+        {
+            using (var file = File.OpenText(filePath))
+            {
+                var testCaseCount = int.Parse(file.ReadLine());
+
+                testCases = new List<TestCase>(testCaseCount);
+
+                for (var i = 0; i < testCaseCount; i++)
+                {
+                    var tokens = ReadDoubles(file);
+                    testCases.Add(new TestCase
+                    {
+                        C = tokens[0],
+                        F = tokens[1],
+                        X = tokens[2]
+                    });
+                }
+            }
+        }
+
+        private readonly List<TestCase> testCases;
+
+        public IEnumerable<TestCase> TestCases
+        {
+            get { return testCases; }
+        }
+
+        private static string[] ReadStrings(TextReader file)
+        {
+            return file.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static double[] ReadDoubles(TextReader file)
+        {
+            return ReadStrings(file).Select(s => double.Parse(s, CultureInfo.InvariantCulture)).ToArray();
+        }
+    }
+}
---
+++ Code Jam 2014/Code Jam 2014 - Qualification Round/CookieClickerAlpha/TestCase.cs
@@ -0,0 +1,37 @@
+﻿// Copyright © Benoit Blanchon 2014
+// All Rights Reserved
+
+namespace CookieClickerAlpha
+{
+    class TestCase
+    {
+        public double C { get; set; }
+        public double F { get; set; }
+        public double X { get; set; }
+
+        public object Solve()
+        {
+            var now = 0.0;
+            var incomes = 2.0;
+
+            while (true)
+            {
+                if (!ShouldBuyFarm(incomes))
+                    return now + X/incomes;
+
+                now += C/incomes;
+                incomes += F;
+            }
+        }
+        
+        bool ShouldBuyFarm(double incomes)
+        {
+            var timeWithoutFarm = X / incomes;
+            var timeToGetFarm = C / incomes;
+            var incomesWithFarm = incomes + F;
+            var timeWithOneFarm = timeToGetFarm + X / incomesWithFarm;
+
+            return timeWithOneFarm < timeWithoutFarm;
+        }
+    }
+}
