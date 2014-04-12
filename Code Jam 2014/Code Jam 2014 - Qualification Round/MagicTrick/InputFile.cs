bd6d2bd0dc18b013cdbf5320286772708e1399bc
Benoit Blanchon <benoit.blanchon@gmail.com>
2014-04-12 13:52:13 +0200

    Added qualification 2014, problem A

---
+++ Code Jam 2014/Code Jam 2014 - Qualification Round/MagicTrick/InputFile.cs
@@ -0,0 +1,63 @@
+﻿// Copyright © Benoit Blanchon 2014
+// All Rights Reserved
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MagicTrick
+{
+    class InputFile
+    {
+        public InputFile(string filePath)
+        {
+            using (var file = File.OpenText(filePath))
+            {
+                var testCaseCount = int.Parse(file.ReadLine());
+
+                testCases = new List<TestCase>(testCaseCount);
+
+                for (var i = 0; i < testCaseCount; i++)
+                {
+                    testCases.Add(new TestCase
+                    {
+                        FirstSelectedRow = ReadSelectedRow(file),
+                        SecondSelectedRow = ReadSelectedRow(file)
+                    });
+                }
+            }
+        }
+
+        static int[] ReadSelectedRow(StreamReader file)
+        {
+            var selectedRowNumber = ReadInts(file)[0];
+
+            for (var j = 1; j < selectedRowNumber; j++)
+                ReadInts(file);
+            var selectedRow = ReadInts(file);
+
+            for (var j = selectedRowNumber; j < 4; j++)
+                ReadInts(file);
+
+            return selectedRow;
+        }
+
+        private readonly List<TestCase> testCases;
+
+        public IEnumerable<TestCase> TestCases
+        {
+            get { return testCases; }
+        }
+
+        private static string[] ReadStrings(TextReader file)
+        {
+            return file.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static int[] ReadInts(TextReader file)
+        {
+            return ReadStrings(file).Select(int.Parse).ToArray();
+        }
+    }
+}
---
+++ Code Jam 2014/Code Jam 2014 - Qualification Round/MagicTrick/TestCase.cs
@@ -0,0 +1,27 @@
+﻿// Copyright © Benoit Blanchon 2014
+// All Rights Reserved
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MagicTrick
+{
+    class TestCase
+    {
+        public IEnumerable<int> FirstSelectedRow { get; set; }
+        public IEnumerable<int> SecondSelectedRow { get; set; }
+
+        public object Solve()
+        {
+            var candidates = FirstSelectedRow.Intersect(SecondSelectedRow).ToArray();
+
+            if (candidates.Length == 0)
+                return "Bad magician!";
+
+            if (candidates.Length > 1)
+                return "Volunteer cheated!";
+
+            return candidates[0];
+        }
+    }
+}
