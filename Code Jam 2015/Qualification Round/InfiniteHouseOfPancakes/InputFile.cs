483375a23e13029be2bed35d89f61363b2787299
Benoit Blanchon <github@benoitblanchon.fr>
2015-04-11 15:35:41 +0200

    First attempt failed. Second attempt.

---
+++ Code Jam 2015/Qualification Round/InfiniteHouseOfPancakes/InputFile.cs
@@ -0,0 +1,55 @@
+﻿// Copyright © Benoit Blanchon 2015
+// All Rights Reserved
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace InfiniteHouseOfPancakes
+{
+    class InputFile
+    {
+        public InputFile(string filePath)
+        {
+            using (var file = File.OpenText(filePath))
+            {
+                var testCaseCount = int.Parse(file.ReadLine());
+
+                testCases = new List<TestCase>(testCaseCount);
+
+                for (var i = 0; i < testCaseCount; i++)
+                {
+                    var dinnersCount = int.Parse(file.ReadLine());
+                    var dinners =  ReadInts(file);
+
+                    Debug.Assert(dinnersCount == dinners.Length);
+
+                    testCases.Add(new TestCase
+                    {
+                        Dinners = dinners
+                    });
+                }
+            }
+        }
+
+        private readonly List<TestCase> testCases;
+
+        public IEnumerable<TestCase> TestCases
+        {
+            get { return testCases; }
+        }
+
+        private static string[] ReadStrings(TextReader file)
+        {
+            return file.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static int[] ReadInts(TextReader file)
+        {
+            return ReadStrings(file).Select(int.Parse).ToArray();
+        }
+    }
+}
---
+++ Code Jam 2015/Qualification Round/InfiniteHouseOfPancakes/TestCase.cs
@@ -0,0 +1,47 @@
+﻿// Copyright © Benoit Blanchon 2015
+// All Rights Reserved
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace InfiniteHouseOfPancakes
+{
+    class TestCase
+    {
+        public IEnumerable<int> Dinners;
+
+        public object Solve()
+        {
+            return GetMinutesFor(Dinners, int.MaxValue);
+        }
+
+        private int GetMinutesFor(IEnumerable<int> dinners, int minutesAllowed)
+        {
+            if (minutesAllowed < 1) return int.MaxValue;
+
+            var minutesWithoutSplit = dinners.Max();
+
+            if (minutesWithoutSplit <= 2) return minutesWithoutSplit;
+            
+            var minutesWithSplit = GetMinutesFor(SplitBiggestDinner(dinners), minutesWithoutSplit - 1) + 1;
+
+            return Math.Min(minutesWithoutSplit, minutesWithSplit);
+        }
+
+        private static IEnumerable<int> SplitBiggestDinner(IEnumerable<int> dinners)
+        {
+            var sortedDinners = new List<int>(dinners);
+            sortedDinners.Sort();
+
+            var biggest = sortedDinners[sortedDinners.Count - 1];
+            Debug.Assert(biggest>1);
+
+            sortedDinners[sortedDinners.Count - 1] -= biggest / 2;
+            sortedDinners.Add(biggest / 2);
+
+            return sortedDinners;
+        }
+    }
+}
