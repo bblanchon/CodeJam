1188ee3d299e877b5c943de977df4ff5f03696e5
Benoit Blanchon <github@benoitblanchon.fr>
2015-04-11 14:16:12 +0200

    First attempt

---
+++ Code Jam 2015/Qualification Round/StandingOvation/InputFile.cs
@@ -0,0 +1,46 @@
+﻿// Copyright © Benoit Blanchon 2015
+// All Rights Reserved
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace StandingOvation
+{
+    class InputFile
+    {
+        public InputFile(string filePath)
+        {
+            using (var file = File.OpenText(filePath))
+            {
+                var testCaseCount = int.Parse(file.ReadLine());
+
+                testCases = new List<TestCase>(testCaseCount);
+
+                for (var i = 0; i < testCaseCount; i++)
+                {
+                    var tokens = file.ReadLine().Split(' ');
+                    var maxShyness = int.Parse(tokens[0]);
+                    var audience = tokens[1].Select(x=>x-'0').ToArray();
+
+                    Debug.Assert(maxShyness+1 == audience.Length);
+                    Debug.Assert(audience.All(x=>x>=0));
+                    Debug.Assert(audience.All(x=>x<=9));
+
+                    testCases.Add(new TestCase
+                    {
+                        Audience = audience
+                    });
+                }
+            }
+        }
+
+        private readonly List<TestCase> testCases;
+
+        public IEnumerable<TestCase> TestCases
+        {
+            get { return testCases; }
+        }  
+    }
+}
---
+++ Code Jam 2015/Qualification Round/StandingOvation/TestCase.cs
@@ -0,0 +1,32 @@
+﻿// Copyright © Benoit Blanchon 2015
+// All Rights Reserved
+
+namespace StandingOvation
+{
+    class TestCase
+    {
+        public int[] Audience;
+
+        public object Solve()
+        {
+            var friendCount = 0;
+            var standingPersonCount = 0;
+
+            for (int shyness = 0; shyness < Audience.Length; shyness++)
+            {
+                if (Audience[shyness] == 0) continue;
+
+                if (standingPersonCount < shyness)
+                {
+                    var additionalFriends = shyness - standingPersonCount;
+                    friendCount += additionalFriends;
+                    standingPersonCount += additionalFriends;
+                }
+
+                standingPersonCount += Audience[shyness];
+            }
+
+            return friendCount;
+        }
+    }
+}
